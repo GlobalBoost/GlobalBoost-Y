ee7c4020bff06ec7a2c4931ab3e298ccaaa39e68
mendozg <35119606+mendozg@users.noreply.github.com>
2023-12-15 09:42:02 -0600

    Max B$TY coin supply 30M
    Correct Max amount of supply for GlobalBoost-Y coins 30Millions or 30,000,000 BSTY

--- src/consensus/amount.h
+++ src/consensus/amount.h
@@ -17,13 +17,13 @@ static constexpr CAmount COIN = 100000000;
 /** No amount larger than this (in satoshi) is valid.
  *
  * Note that this constant is *not* the total money supply, which in GlobalBoost
- * currently happens to be less than 21,000,000 BSTY for various reasons, but
+ * currently happens to be less than 30,000,000 BSTY for various reasons, but
  * rather a sanity check. As this sanity check is used by consensus-critical
  * validation code, the exact value of the MAX_MONEY constant is consensus
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static constexpr CAmount MAX_MONEY = 24000000 * COIN;
+static constexpr CAmount MAX_MONEY = 30000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif // GLOBALBOOST_CONSENSUS_AMOUNT_H
